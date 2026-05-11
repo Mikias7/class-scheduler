2a49ea95ba4866b512e12300f4014afae73097a6
Mikias7 <mikias7t@gmail.com>
2026-05-11 07:36:51 -0500

    change nav text

--- class-scheduler/src/components/Nav.tsx
+++ class-scheduler/src/components/Nav.tsx
@@ -8,7 +8,7 @@ function Nav() {
       {/* Logo */}
       <div className="flex items-center gap-3">
         <img src={logo} alt="logo" className="h-12 w-auto" />
-        <h1 className="text-xl font-semibold text-[#FFD700]">Academic Planner</h1>
+        <h1 className="text-xl font-semibold text-[#FFD700]">Course Scheduler</h1>
       </div>
       
     </nav>
